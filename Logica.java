38dbdaac14a3f7d928400109b666d54080817861
Juliana Portela <julianaportehla@hotmail.com>
2022-06-08 15:26:56 -0300

    Testes

---
+++ Logica.java
@@ -0,0 +1,14 @@
+public class Logica {
+
+    public static void main(String[] args){
+        String nome = "Ju";
+        int idade = 27;
+        double peso = 60.0;
+
+        System.out.println("O meu nome é " + nome);
+        System.out.println("A minha idade é " + idade);
+        System.out.println("O meu peso é " + peso);
+
+        
+    }
+}
